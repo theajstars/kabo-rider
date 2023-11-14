54a897e9406785b214474b8f1e41b6c1d6966c78
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2023-11-14 04:39:56 +0100

    :construction:

--- src/Pages/Notifications/index.tsx
+++ src/Pages/Notifications/index.tsx
@@ -6,7 +6,7 @@ import {
   Alert,
   AlertTitle,
   Button,
-  Grid,
+  Modal,
   Paper,
   Table,
   TableContainer,
@@ -23,13 +23,19 @@ import MegaLoader from "../../Misc/MegaLoader";
 import { AppContext } from "../DashboardContainer";
 import { PerformRequest, usePerformRequest } from "../../Lib/PerformRequest";
 import { Endpoints } from "../../Lib/Endpoints";
-import { GetProductsResponse, NonPaginatedResponse } from "../../Lib/Responses";
+import {
+  GetOrdersResponse,
+  GetProductsResponse,
+  NonPaginatedResponse,
+} from "../../Lib/Responses";
 import {
   Notification as RiderNotification,
   NotificationResponse,
+  Order,
   Product,
   RiderStats,
 } from "../../Lib/Types";
+import ProgressCircle from "../../Misc/ProgressCircle";
 
 export default function Notifications() {
   const navigate = useNavigate();
@@ -51,19 +57,46 @@ export default function Notifications() {
   }, [riderContext]);
 
   const [isLoading, setLoading] = useState<boolean>(false);
+  const [isOrderLoading, setOrderLoading] = useState<boolean>(false);
+  const [isOrderAccepting, setOrderAccepting] = useState<boolean>(false);
 
-  const { data: riderNotifications } = usePerformRequest<
-    NotificationResponse,
-    NonPaginatedResponse<NotificationResponse>
-  >({
-    method: "POST",
-    url: Endpoints.GetNotifications,
-    body: { token: Cookies.get("token") },
-  });
+  const { data: riderNotifications, isLoading: isNotificationsLoading } =
+    usePerformRequest<
+      NotificationResponse,
+      NonPaginatedResponse<NotificationResponse>
+    >({
+      method: "POST",
+      url: Endpoints.GetNotifications,
+      body: { token: Cookies.get("token") },
+    });
   console.log(riderNotifications?.data.orders);
 
-  const [currentOrder, setCurrentOrder] = useState<RiderNotification | null>();
+  const [currentOrder, setCurrentOrder] = useState<Order | null>(null);
   const [isOrderModalVisible, setOrderModalVisible] = useState<boolean>(false);
+
+  const getOrderDetails = async (referenceCode: string) => {
+    removeAllToasts();
+    setOrderLoading(true);
+    const r: GetOrdersResponse = await PerformRequest({
+      method: "POST",
+      route: Endpoints.GetOrders,
+      data: {
+        token: Cookies.get("token"),
+        reference_code: referenceCode,
+        account: "rider",
+      },
+    }).catch(() => {
+      setOrderLoading(false);
+      addToast("An error occurred!", { appearance: "error" });
+    });
+    setOrderLoading(false);
+    if (r && r.data && r.data.status === "success") {
+      setCurrentOrder(r.data.data[0]);
+    }
+  };
+
+  const acceptOrder = async () => {};
+
   return (
     <div
       className="notifications-container flex-col width-100"
@@ -77,43 +110,173 @@ export default function Notifications() {
           <div className="flex-col width-100 align-center justify-center notifications">
             <span className="px-17 fw-500">Pending Orders</span>
           </div>
+
           <br />
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Reference Code</TableCell>
-                  <TableCell>Address</TableCell>
-                  <TableCell>Details</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {riderNotifications?.data.orders.map((order) => {
-                  return (
-                    <TableRow key={order.reference_code}>
-                      <TableCell>{order.reference_code}</TableCell>
-                      <TableCell>{order.address}</TableCell>
-                      <TableCell>
-                        <Button
-                          sx={{
-                            fontSize: "13px",
-                          }}
-                          onClick={() => {
-                            setCurrentOrder(order);
-                            setOrderModalVisible(true);
-                          }}
-                          variant="contained"
-                          color="primary"
-                        >
-                          View Details
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
+
+          {isNotificationsLoading ? (
+            <div className="flex-row width-100 justify-center align-center">
+              <ProgressCircle />
+            </div>
+          ) : (
+            <>
+              {riderNotifications?.data.orders.length === 0 ? (
+                <Alert severity="info" sx={{ width: "100%", padding: "10px" }}>
+                  <AlertTitle>There are no notifications</AlertTitle>
+                </Alert>
+              ) : (
+                <>
+                  <TableContainer component={Paper}>
+                    <Table>
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Reference Code</TableCell>
+                          <TableCell>Address</TableCell>
+                          <TableCell>Details</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {riderNotifications?.data.orders.map((order) => {
+                          return (
+                            <TableRow key={order.reference_code}>
+                              <TableCell>{order.reference_code}</TableCell>
+                              <TableCell>{order.address}</TableCell>
+                              <TableCell>
+                                <Button
+                                  sx={{
+                                    fontSize: "13px",
+                                  }}
+                                  onClick={() => {
+                                    getOrderDetails(order.reference_code);
+                                    setOrderModalVisible(true);
+                                  }}
+                                  variant="contained"
+                                  color="primary"
+                                >
+                                  View Details
+                                </Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                  <Modal
+                    open={isOrderModalVisible}
+                    onClose={() => {
+                      setOrderModalVisible(false);
+                    }}
+                  >
+                    <div className="notification-modal flex-col">
+                      <div className="flex-row width-100 align-center justify-center">
+                        <span className="px-14 fw-600">Order Details</span>
+                      </div>
+                      {isOrderLoading ? (
+                        <ProgressCircle />
+                      ) : (
+                        <>
+                          {currentOrder && (
+                            <>
+                              <div className="flex-row modal-row">
+                                <span className="px-15 fw-600">
+                                  Reference Code
+                                </span>
+
+                                <span className="px-15 fw-400">
+                                  {currentOrder.reference_code}
+                                </span>
+                              </div>
+                              <div className="flex-row modal-row">
+                                <span className="px-15 fw-600">Users</span>
+                                {currentOrder.customer.map((c) => {
+                                  return (
+                                    <span>
+                                      <span className="px-15 fw-400">
+                                        {c.othernames}
+                                      </span>
+                                      &nbsp;
+                                      <span className="px-15 fw-400">
+                                        {c.lastname}
+                                      </span>
+                                      &nbsp; - &nbsp;
+                                      <span className="px-15 fw-400">
+                                        {c.phone}
+                                      </span>
+                                    </span>
+                                  );
+                                })}
+                              </div>
+                              <div className="flex-row modal-row">
+                                <span className="px-15 fw-600">Stores</span>
+                                {currentOrder.store.map((c) => {
+                                  return (
+                                    <span>
+                                      <span className="px-15 fw-400">
+                                        {c.name}
+                                      </span>
+                                    </span>
+                                  );
+                                })}
+                              </div>
+                              <div className="flex-row modal-row">
+                                <span className="px-15 fw-600">Products</span>
+                                <table border={1}>
+                                  <thead>
+                                    <tr>
+                                      <th>Name</th>
+                                      <th>Quantity</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    {currentOrder.product.map((p) => {
+                                      return (
+                                        <tr>
+                                          <td>{p.name}</td>
+                                          <td>{p.quantity}</td>
+                                        </tr>
+                                      );
+                                    })}
+                                  </tbody>
+                                </table>
+                              </div>
+                              <div className="flex-row modal-row">
+                                <span className="px-15 fw-600">
+                                  Shipping details
+                                </span>
+                                {currentOrder.shipping.map((c) => {
+                                  return (
+                                    <span className="flex-col">
+                                      <span className="px-15 fw-400">
+                                        {c.address}
+                                      </span>
+                                      <span className="px-15 fw-400">
+                                        {c.details}
+                                      </span>
+                                    </span>
+                                  );
+                                })}
+                              </div>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                sx={{ height: "40px", fontSize: "14px" }}
+                              >
+                                {isOrderAccepting ? (
+                                  <ProgressCircle />
+                                ) : (
+                                  "Accept Order"
+                                )}
+                              </Button>
+                            </>
+                          )}
+                        </>
+                      )}
+                    </div>
+                  </Modal>
+                </>
+              )}
+            </>
+          )}
           <br />
         </>
       ) : (
